18c14e5b54b2f2c88e318ef0ee1182599a870efb
spadix0 <spadix@users.sourceforge.net>
2021-01-07 12:29:21 +0000

    update to zig master
    string formatting should use 's' specifier;
    unspecified appears to dump a debug representation now

--- build.zig
+++ build.zig
@@ -1,30 +1,29 @@
 const std = @import("std");
-const Builder = std.build.Builder;
 
 var regress: *std.build.Step = undefined;
 
 
-pub fn build(b: *Builder) void {
+pub fn build(b: *std.build.Builder) void {
     regress = b.step("regress", "Run tests for all days");
 
     addexe(b, 1, "fuel");
 }
 
 
-pub fn addexe(b: *Builder, iday: u32, name: []const u8) void {
+pub fn addexe(b: anytype, iday: u32, name: []const u8) void {
     const day = b.fmt("{d:0>2}", .{ iday });
-    const src = b.fmt("{}/{}.zig", .{ day, name });
+    const src = b.fmt("{s}/{s}.zig", .{ day, name });
 
     const test_ = b.addTest(src);
     test_.setBuildMode(b.standardReleaseOptions());
     test_.addPackagePath("aoc", "aoc.zig");
     regress.dependOn(&test_.step);
     b.step(
-            b.fmt("test{}", .{ day }),
-            b.fmt("Run tests for day {} ({})", .{ iday, name }))
+            b.fmt("test{s}", .{ day }),
+            b.fmt("Run tests for day {d} ({s})", .{ iday, name }))
         .dependOn(&test_.step);
 
-    const exe = b.addExecutable(b.fmt("{}-{}", .{ day, name }), src);
+    const exe = b.addExecutable(b.fmt("{s}-{s}", .{ day, name }), src);
     exe.setBuildMode(b.standardReleaseOptions());
     exe.addPackagePath("aoc", "aoc.zig");
     exe.install();
@@ -35,7 +34,7 @@ pub fn addexe(b: *Builder, iday: u32, name: []const u8) void {
         run.addArgs(args);
 
     b.step(
-            b.fmt("run{}", .{ day }),
-            b.fmt("Run day {} ({})", .{ iday, name }))
+            b.fmt("run{s}", .{ day }),
+            b.fmt("Run day {d} ({s})", .{ iday, name }))
         .dependOn(&run.step);
 }
